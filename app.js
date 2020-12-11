a56676a4774229d2ff104cecf79d7741ca8a47da
Nurislam <nurislamvaleev@gmail.com>
2020-12-11 18:08:00 +0300

    added model array

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const model = [
 	{ type: 'title', value: 'Hello World from JS' },
 	{ type: 'text', value: 'here we go with some text' },
-	{ type: 'columns' },
+	{ type: 'columns', value: ['11111', '22222', '33333'] },
 ];
+
+const $site = document.querySelector('#site');
+
+model.forEach((block) => {
+	let html = '';
+
+	if (block.type === 'title') {
+		html = `
+		<div class="row">
+			<div class="col-sm">
+				<h1>${block.value}</h1>
+			</div>
+		</div>
+		`;
+	} else if (block.type === 'text') {
+		html = `
+		<div class="row">
+			<div class="col-sm">
+				<p>${block.value}</p>
+			</div>
+		</div>
+		`;
+	} else if (block.type === 'columns') {
+		///////
+	}
+
+	$site.insertAdjacentHTML('beforeend', html);
+});
